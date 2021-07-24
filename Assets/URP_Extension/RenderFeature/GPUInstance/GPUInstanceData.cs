b63b9af8870c7d12dd323f023b996aacd895b273
Bioumaiquan <shengwanle@gmail.com>
2021-07-24 17:48:02 +0800

    gpu instance

---
+++ Assets/URP_Extension/RenderFeature/GPUInstance/GPUInstanceData.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+[ExecuteAlways]
+public class GPUInstanceData : MonoBehaviour
+{
+    [Serializable]
+    public class InstanceTile
+    {
+        public Vector4 tileMinMax;
+        public Matrix4x4[] localToWorld;
+        public int BatchCount = 1;
+    }
+
+    [SerializeField] private InstanceTile[] m_InstanceTiles;
+    public InstanceTile[] InstanceTiles => m_InstanceTiles;
+
+    [SerializeField] private Material m_InstanceMaterial;
+    public Material InstanceMaterial => m_InstanceMaterial;
+    
+    [SerializeField] private Mesh m_InstanceMesh;
+    public Mesh InstanceMesh => m_InstanceMesh;
+    
+    [SerializeField] private int m_CountPerInstance = 500;
+    public int CountPerInstance => m_CountPerInstance;
+    
+    
+    public static GPUInstanceData Instance;
+    public bool bake;
+
+    
+    private void OnEnable()
+    {
+        Instance = this;
+    }
+
+    private int BoxSize = 5;
+    
+    private void Bake()
+    {
+        int totalCount = transform.childCount;
+        if (totalCount == 0)
+            return;
+        
+        for (int k = 0; k < totalCount; k++)
+        {
+            transform.GetChild(k).gameObject.SetActive(true);
+        }
+
+        GameObject instanceGO = transform.GetChild(0).gameObject;
+        m_InstanceMesh = instanceGO.GetComponent<MeshFilter>().sharedMesh;
+        m_InstanceMaterial = instanceGO.GetComponent<MeshRenderer>().sharedMaterial;
+        
+
+        Vector4 minMaxPos = CalculateMinMaxPosition(transform);
+        int BoxCountX = (int)(minMaxPos.z - minMaxPos.x) / BoxSize + 1;
+        int BoxCountZ = (int)(minMaxPos.w - minMaxPos.y) / BoxSize + 1;
+
+        GameObject box = new GameObject("box");
+        box.AddComponent<BoxCollider>();
+        box.transform.localScale = Vector3.one * BoxSize;
+
+        m_InstanceTiles = new InstanceTile[BoxCountX * BoxCountZ];
+        Vector3 pos = Vector3.zero;
+        for (int i = 0; i < BoxCountX; i++)
+        {
+            pos.x = minMaxPos.x + BoxSize * 0.5f + i * BoxSize;
+            for (int j = 0; j < BoxCountZ; j++)
+            {
+                pos.z = minMaxPos.y + BoxSize * 0.5f + j * BoxSize;
+                GameObject.Instantiate(box, pos, Quaternion.identity);
+            }
+        }
+        
+
+        for (int k = 0; k < totalCount; k++)
+        {
+            transform.GetChild(k).gameObject.SetActive(false);
+        }
+    }
+    
+    
+    Vector4 CalculateMinMaxPosition(Transform transform)
+    {
+        Vector4 MinMaxPosition = Vector4.zero;
+        int maxCount = transform.childCount;
+        for (int i = 0; i < maxCount; i++)
+        {
+            Vector3 position = transform.GetChild(i).position;
+            if (position.x < MinMaxPosition.x)
+                MinMaxPosition.x = position.x;
+            else if (position.x > MinMaxPosition.z)
+                MinMaxPosition.z = position.x;
+            
+            if (position.z < MinMaxPosition.y)
+                MinMaxPosition.y = position.z;
+            else if (position.z > MinMaxPosition.w)
+                MinMaxPosition.w = position.z;
+        }
+
+        return MinMaxPosition;
+    }
+
+    private void Update()
+    {
+        if (bake)
+        {
+            Bake();
+            bake = false;
+        }
+    }
+}
---
+++ Assets/URP_Extension/RenderFeature/GPUInstance/GPUInstanceRenderFeature.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Serialization;
+
+public class GPUInstanceRenderFeature : ScriptableRendererFeature
+{
+    public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+    
+    GPUInstanceRenderPass scriptablePass;
+    public override void Create()
+    {
+        scriptablePass = new GPUInstanceRenderPass("GPU Instance");
+        scriptablePass.renderPassEvent = renderPassEvent;
+        scriptablePass.localToWorldList = new List<Matrix4x4[]>();
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(scriptablePass);
+    }
+
+
+    class GPUInstanceRenderPass : ScriptableRenderPass
+    {
+        string profilerTag;
+        public List<Matrix4x4[]> localToWorldList;
+        
+        public GPUInstanceRenderPass(string profilerTag)
+        {
+            this.profilerTag = profilerTag;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            Camera camera = renderingData.cameraData.camera;
+            if (camera.cameraType != CameraType.Game && camera.cameraType != CameraType.SceneView)
+                return;
+            
+            GPUInstanceData.InstanceTile[] tiles = GPUInstanceData.Instance.InstanceTiles;
+
+            if (tiles == null)
+                return;
+            
+            Material instanceMaterial = GPUInstanceData.Instance.InstanceMaterial;
+            Mesh instanceMesh = GPUInstanceData.Instance.InstanceMesh;
+            int countPerInstance = GPUInstanceData.Instance.CountPerInstance;
+            
+
+            CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
+
+            
+            for (int i = 0; i < tiles.Length; i++)
+            {
+                int totalCount = tiles[i].localToWorld.Length;
+
+                if (tiles[i].BatchCount == 1)
+                {
+                    cmd.DrawMeshInstanced(instanceMesh, 0, instanceMaterial, 0, tiles[i].localToWorld);
+                }
+                else
+                {
+                    localToWorldList.Clear();
+                    
+                    //tile内的总mesh数量可能大于合批数量  所以先拆分数组
+                    
+                    //前几批的数组
+                    int j;
+                    for (j = 0; j < tiles[i].BatchCount - 1; j++)
+                    {
+                        Matrix4x4[] matrix4x4s = new Matrix4x4[countPerInstance];
+                        Array.Copy(tiles[i].localToWorld, j * countPerInstance, matrix4x4s, 0, countPerInstance);
+                        localToWorldList.Add(matrix4x4s);
+                    }
+                    
+                    //最后一个批次的矩阵数组
+                    int lastCount = totalCount - (tiles[i].BatchCount - 1) * countPerInstance;
+                    if (lastCount > 0)
+                    {
+                        Matrix4x4[] lastMatrix4x4s = new Matrix4x4[lastCount];
+                        Array.Copy(tiles[i].localToWorld, j * countPerInstance, lastMatrix4x4s, 0, lastCount);
+                        localToWorldList.Add(lastMatrix4x4s);
+                    }
+
+                    //渲染
+                    for (int k = 0; k < localToWorldList.Count; k++)
+                    {
+                        cmd.DrawMeshInstanced(instanceMesh, 0, instanceMaterial, 0, localToWorldList[k]);
+                    }
+                }
+            }
+
+
+            context.ExecuteCommandBuffer(cmd);
+            cmd.Clear();
+            CommandBufferPool.Release(cmd);
+        }
+
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+        }
+    }
+}
